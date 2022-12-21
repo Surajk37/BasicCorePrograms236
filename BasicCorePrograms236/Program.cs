c2aebbe6703f80ae24204763734b1da5f7225238
Surajk37 <surajkhot9993@gmail.com>
2022-12-21 13:23:30 +0530

    [SURAJ] Added a Greeting Message

---
+++ BasicCorePrograms236/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicCorePrograms236
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t Welcome To Basic Core Programs\n");
+            Console.ReadLine();
+        }
+    }
+}
