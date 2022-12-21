3c352dba84bf0a1c055ef7ac916d7bb4a58a0034
Surajk37 <surajkhot9993@gmail.com>
2022-12-21 14:34:41 +0530

    [SURAJ] Calculated a percentage of tails and head

---
+++ BasicCorePrograms236/FlipCoin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms236
+{
+    internal class FlipCoin
+    {
+        /// <summary>
+        /// Finding the percentage of Tails and Head.
+        /// </summary>
+        /// <param name="n"></param>
+        public static void FindPercentage(int n)
+        {
+            int tailCount = 0,headCount=0;
+            Random random = new Random();
+
+            for(int i = 1; i <= n; i++)
+            {
+                double input = random.NextDouble();
+                if(input < 0.5)
+                    tailCount++;
+                else
+                    headCount++;
+                Console.WriteLine("Random Values are:{0}",input);
+            }
+            Console.WriteLine("\nTail Count: {0}   Head Count: {1}",tailCount,headCount);
+
+            double TailPercentage = (tailCount * 100) / n;
+            double HeadPercentage = (headCount * 100) / n;
+            Console.WriteLine("Percentage of Tails : {0}",TailPercentage);
+            Console.WriteLine("Percentage of Head : {0}", HeadPercentage);
+        }
+    }
+}
--- BasicCorePrograms236/Program.cs
+++ BasicCorePrograms236/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t Welcome To Basic Core Programs\n");
+
+            Console.WriteLine("Please choose a option from Below List");
+            Console.WriteLine("1. FlipCoin");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("\tFlip Coin Program");
+                    Console.WriteLine("Enter a Number of Time to Flip the Coin");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    FlipCoin.FindPercentage(n);
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+            }
             Console.ReadLine();
         }
     }
